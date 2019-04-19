6a70dbeb847634a4c4b2c7be238ca274d605a6f6
Andrea Ambrosini - Rossonet s.c.a r.l <andrea.ambrosini@rossonet.org>
2019-04-19 03:27:38 +0200

    avanzamenti web interface

--- reactor-web/ui/src/pages/Dashboard.vue
+++ reactor-web/ui/src/pages/Dashboard.vue
@@ -182,87 +182,87 @@ export default {
         url: '/ar4k/dashtable/env.html',
         dataType: 'html',
         success: function (data) {
-          $('#nav-tabs-enviroment').html(data)
+          window.jQuery('#nav-tabs-enviroment').html(data)
         }
-      }),
+      })
       window.jQuery.ajax({
         url: '/ar4k/dashtable/conf.html',
         dataType: 'html',
         success: function (data) {
-          $('#nav-tabs-configuration').html(data)
+          window.jQuery('#nav-tabs-configuration').html(data)
         }
-      }),
+      })
       window.jQuery.ajax({
         url: '/ar4k/dashtable/web.html',
         dataType: 'html',
         success: function (data) {
-          $('#nav-tabs-web-mapping').html(data)
+          window.jQuery('#nav-tabs-web-mapping').html(data)
         }
-      }),
+      })
       window.jQuery.ajax({
         url: '/ar4k/dashtable/logger.html',
         dataType: 'html',
         success: function (data) {
-          $('#nav-tabs-logger').html(data)
+          window.jQuery('#nav-tabs-logger').html(data)
         }
-      }),
+      })
       window.jQuery.ajax({
         url: '/ar4k/dashtable/metrics.html',
         dataType: 'html',
         success: function (data) {
-          $('#nav-tabs-metrics').html(data)
+          window.jQuery('#nav-tabs-metrics').html(data)
         }
-      }),
+      })
       window.jQuery.ajax({
         url: '/ar4k/dashtable/blockchain.html',
         dataType: 'html',
         success: function (data) {
-          $('#nav-tabs-block-chain').html(data)
+          window.jQuery('#nav-tabs-block-chain').html(data)
         }
-      }),
+      })
       window.jQuery.ajax({
         url: '/ar4k/dashtable/keystores.html',
         dataType: 'html',
         success: function (data) {
-          $('#nav-tabs-key-stores').html(data)
+          window.jQuery('#nav-tabs-key-stores').html(data)
         }
-      }),
+      })
       window.jQuery.ajax({
         url: '/control/health',
         dataType: 'json',
         success: function (data) {
-          $('#hw_ram').html(Math.round((data.details.ar4k.details['hardware-runtime-total-memory'] - data.details.ar4k.details['hardware-runtime-free-memory']) / (1024 * 1024)) + ' / ' + Math.round(data.details.ar4k.details['hardware-runtime-total-memory'] / (1024 * 1024)) + '<small> MB</small>')
-          $('#hw_status').html(data.status)
-          $('#hw_disk').html(parseFloat((data.details.diskSpace.details['total'] - data.details.diskSpace.details['free']) / (1024 * 1024 * 1024)).toFixed(2) + ' / ' + parseFloat(data.details.diskSpace.details['total'] / (1024 * 1024 * 1024)).toFixed(2) + '<small> GB</small>')
-          $('#hw_cpu').html(parseFloat(100 * data.details.ar4k.details['system-load-average'] / (data.details.ar4k.details['pi-Processor'])).toFixed(1) + '<small> %</small><br/><small>' + data.details.ar4k.details['pi-CPU-Model-Name'] + '</small>')
-          $('#ar4k_net_hostname').html(data.details.ar4k.details['operating-system'].networkParams['hostName'])
-          $('#ar4k_net_ip').html(data.details.ar4k.details['pi-IP-Addresses-0'] + '  ' + (data.details.ar4k.details['pi-IP-Addresses-1'] || '') + '  ' + (data.details.ar4k.details['pi-IP-Addresses-2'] || '') + '  ' + (data.details.ar4k.details['pi-IP-Addresses-3'] || ''))
-          $('#ar4k_net_dns').html(data.details.ar4k.details['operating-system'].networkParams['dnsServers'].join(' '))
-          $('#ar4k_net_domain').html('<small>' + data.details.ar4k.details['operating-system'].networkParams['domainName'] + '</small>')
-          $('#ar4k_net_gw').html(data.details.ar4k.details['operating-system'].networkParams['ipv4DefaultGateway'])
+          window.jQuery('#hw_ram').html(Math.round((data.details.ar4k.details['hardware-runtime-total-memory'] - data.details.ar4k.details['hardware-runtime-free-memory']) / (1024 * 1024)) + ' / ' + Math.round(data.details.ar4k.details['hardware-runtime-total-memory'] / (1024 * 1024)) + '<small> MB</small>')
+          window.jQuery('#hw_status').html(data.status)
+          window.jQuery('#hw_disk').html(parseFloat((data.details.diskSpace.details['total'] - data.details.diskSpace.details['free']) / (1024 * 1024 * 1024)).toFixed(2) + ' / ' + parseFloat(data.details.diskSpace.details['total'] / (1024 * 1024 * 1024)).toFixed(2) + '<small> GB</small>')
+          window.jQuery('#hw_cpu').html(parseFloat(100 * data.details.ar4k.details['system-load-average'] / (data.details.ar4k.details['pi-Processor'])).toFixed(1) + '<small> %</small><br/><small>' + data.details.ar4k.details['pi-CPU-Model-Name'] + '</small>')
+          window.jQuery('#ar4k_net_hostname').html(data.details.ar4k.details['operating-system'].networkParams['hostName'])
+          window.jQuery('#ar4k_net_ip').html(data.details.ar4k.details['pi-IP-Addresses-0'] + '  ' + (data.details.ar4k.details['pi-IP-Addresses-1'] || '') + '  ' + (data.details.ar4k.details['pi-IP-Addresses-2'] || '') + '  ' + (data.details.ar4k.details['pi-IP-Addresses-3'] || ''))
+          window.jQuery('#ar4k_net_dns').html(data.details.ar4k.details['operating-system'].networkParams['dnsServers'].join(' '))
+          window.jQuery('#ar4k_net_domain').html('<small>' + data.details.ar4k.details['operating-system'].networkParams['domainName'] + '</small>')
+          window.jQuery('#ar4k_net_gw').html(data.details.ar4k.details['operating-system'].networkParams['ipv4DefaultGateway'])
         }
       })
       window.jQuery.ajax({
         url: '/ar4k/status',
         dataType: 'json',
         success: function (data) {
-          $('#ar4k_config_label').html(data['run-config'].name + '<br/><small>' + data['run-config'].description + '</small>')
-          $('#ar4k_runlevel').html(data['run-level'])
-          $('#ar4k_services_count').html(data['run-services'].length)
+          window.jQuery('#ar4k_config_label').html(data['run-config'].name + '<br/><small>' + data['run-config'].description + '</small>')
+          window.jQuery('#ar4k_runlevel').html(data['run-level'])
+          window.jQuery('#ar4k_services_count').html(data['run-services'].length)
         }
       })
       window.jQuery.ajax({
         url: '/control/beans',
         dataType: 'json',
         success: function (data) {
-          $('#ar4k_beans_count').html(Object.keys(data.contexts.application.beans).length)
+          window.jQuery('#ar4k_beans_count').html(Object.keys(data.contexts.application.beans).length)
         }
       })
       window.jQuery.ajax({
         url: '/control/threaddump',
         dataType: 'json',
         success: function (data) {
-          $('#ar4k_threads_count').html(data.threads.length)
+          window.jQuery('#ar4k_threads_count').html(data.threads.length)
         }
       })
       window.jQuery.ajax({
@@ -270,10 +270,10 @@ export default {
         dataType: 'json',
         success: function (data) {
           if (typeof data.git !== 'undefined') {
-            $('#ar4k_build').html(data.app.name + ' ' + (data.git.build.version || 'dev') + ' (' + (data.git.build.time || 'no git info') + ')<br/><small>' + data.app.description + '</small')
-            $('#ar4k_builder').html((data.git.build.host || '') + '<br/><small>' + (data.git.build.user.name || '') + '<br/>(' + (data.git.build.user.email || '') + ')</small>')
-            $('#ar4k_commit').html('<small>' + (data.git.commit.time || 'now') + '<br/>' + (data.git.commit.message['short'] || '') + '</small><br/>' + (data.git.commit.id['abbrev'] || ''))
-            $('#ar4k_author').html((data.git.commit.user.name || 'you'))
+            window.jQuery('#ar4k_build').html(data.app.name + ' ' + (data.git.build.version || 'dev') + ' (' + (data.git.build.time || 'no git info') + ')<br/><small>' + data.app.description + '</small')
+            window.jQuery('#ar4k_builder').html((data.git.build.host || '') + '<br/><small>' + (data.git.build.user.name || '') + '<br/>(' + (data.git.build.user.email || '') + ')</small>')
+            window.jQuery('#ar4k_commit').html('<small>' + (data.git.commit.time || 'now') + '<br/>' + (data.git.commit.message['short'] || '') + '</small><br/>' + (data.git.commit.id['abbrev'] || ''))
+            window.jQuery('#ar4k_author').html((data.git.commit.user.name || 'you'))
           }
         }
       })
