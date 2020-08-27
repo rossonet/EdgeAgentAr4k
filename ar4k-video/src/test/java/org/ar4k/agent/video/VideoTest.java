cccf27cd592fbebc7c9e517a903d27dbe61a7081
Andrea Ambrosini - Rossonet s.c.a r.l <andrea.ambrosini@rossonet.org>
2020-08-28 01:31:56 +0200

    correzioni configurazioni test video

--- ar4k-video/src/test/java/org/ar4k/agent/video/VideoTest.java
+++ ar4k-video/src/test/java/org/ar4k/agent/video/VideoTest.java
@@ -25,6 +25,7 @@ import org.ar4k.agent.spring.EdgeAuthenticationManager;
 import org.ar4k.agent.spring.EdgekuserDetailsService;
 import org.jline.builtins.Commands;
 import org.junit.After;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -58,7 +59,7 @@ import com.github.sarxos.webcam.WebcamPanel;
 @TestPropertySource(locations = "classpath:application-base.properties")
 @SpringBootConfiguration
 @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
-//@Ignore
+@Ignore
 public class VideoTest implements WebcamMotionListener, WebcamPanel.Painter {
 
 	@Autowired
