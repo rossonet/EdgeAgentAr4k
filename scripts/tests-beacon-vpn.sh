2d41b8b6eb2b3cf22383759541d151e4312743aa
Andrea Ambrosini - Rossonet s.c.a r.l <andrea.ambrosini@rossonet.org>
2020-09-06 02:10:01 +0200

    refactor sessioni in tunnel

--- scripts/tests-beacon-vpn.sh
+++ scripts/tests-beacon-vpn.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # by Ambrosini
-./gradlew :ar4k-agent-small:test --tests "org.ar4k.agent.full.BeaconClientServerNoSslTests"
+./gradlew :ar4k-agent-small:test --tests "org.ar4k.agent.full.BeaconClientServerNoSslTests.ante_c_serverAndClientTestWithFork"
