7ace139273323e204ef4810e3759e98ad0667ec2
Andrea Ambrosini <andrea.ambrosini@rossonet.org>
2021-06-23 14:37:56 +0200

    fix script javadoc

--- scripts/genera-documentazione.sh
+++ scripts/genera-documentazione.sh
@@ -4,7 +4,7 @@ scripts/rigenerate_todo.sh
 echo "Gradle clean"
 ./gradlew clean
 echo "Gradle javadoc"
-./gradlew ar4kAllJavaDoc  -Psigning.password=${{ secrets.OSSRH_GPG_SECRET_KEY_PASSWORD }} -Psigning.keyId=F6113733 -Psigning.secretKeyRingFile=/home/runner/.gnupg/private.key -Dorg.gradle.jvmargs="-Xms512M -Xmx4G" -Dorg.gradle.daemon=false > /dev/null 
+./gradlew ar4kAllJavaDoc  -Psigning.password=$OSSRH_GPG_SECRET_KEY_PASSWORD -Psigning.keyId=F6113733 -Psigning.secretKeyRingFile=/home/runner/.gnupg/private.key -Dorg.gradle.jvmargs="-Xms512M -Xmx4G" -Dorg.gradle.daemon=false > /dev/null 
 cp -LR mkdocs/* build/docs
 echo "Genera il sito con MkDoc"
 cd build
