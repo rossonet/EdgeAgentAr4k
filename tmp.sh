ad1f2d1f99777728314d95c3b7af54193bd32be1
Andrea Ambrosini <andrea.ambrosini@rossonet.org>
2021-07-02 16:42:00 +0200

    clean vaadin build

---
+++ tmp.sh
@@ -0,0 +1,5 @@
+git rm ar4k-agent-*/package.json
+git rm ar4k-agent-*/pnpm-lock.yaml
+git rm ar4k-agent-*/pnpmfile.js
+git rm ar4k-agent-*/webpack.config.js
+git rm ar4k-agent-*/webpack.generated.js
