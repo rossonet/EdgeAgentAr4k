7263d72532b0f64f4ffcc1874638f4829a475691
Andrea Ambrosini <andrea.ambrosini@rossonet.org>
2021-08-30 21:51:27 +0200

    rpm build -fix-

--- scripts/make-rpm-small-and-publish.sh
+++ scripts/make-rpm-small-and-publish.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-echo clean /build/yum-repo
-rm -r build/yum-repo
-echo create /build/yum-repo
-mkdir -p build/yum-repo
-echo copy fep rpm
-cp build/distributions/ar4k-agent-small-*.noarch.rpm build/yum-repo/ar4k-console.noarch.rpm
+echo clean /build/yum-ar4k-repo
+rm -r build/yum-ar4k-repo
+echo create /build/yum-ar4k-repo
+mkdir -p build/yum-ar4k-repo
+echo copy ar4k console rpm
+cp build/distributions/ar4k-agent-small-*.noarch.rpm build/yum-ar4k-repo/ar4k-console.noarch.rpm
 echo createrepo command
-createrepo --database build/yum-repo
+createrepo --database build/yum-ar4k-repo
 echo yum repository created
 echo compress repository
 cd build
-tar -czf yum-repo.tgz yum-repo
+tar -czf yum-ar4k-repo.tgz yum-ar4k-repo
 echo send repository to web.rossonet.net
 eval $(ssh-agent -s)
 echo "$ROSSOENT_KEY" | ssh-add -
-scp -P 6666 -o StrictHostKeyChecking=no yum-repo.tgz rpm-ar4k@web.rossonet.net:/var/www/html/dati/ar4k-repo/yum-repo.tgz
+scp -P 6666 -o StrictHostKeyChecking=no yum-ar4k-repo.tgz rpm-ar4k@web.rossonet.net:/var/www/html/dati/ar4k-repo/yum-ar4k-repo.tgz
 echo clean and deploy on web.rossonet.net
-ssh -p 6666 -o StrictHostKeyChecking=no rpm-ar4k@web.rossonet.net 'cd /var/www/html/dati/ar4k-repo && rm -rf yum-repo && tar -xzf yum-repo.tgz' 
+ssh -p 6666 -o StrictHostKeyChecking=no rpm-ar4k@web.rossonet.net 'cd /var/www/html/dati/ar4k-repo && rm -rf yum-ar4k-repo && tar -xzf yum-ar4k-repo.tgz' 
 echo deploy completed
 
